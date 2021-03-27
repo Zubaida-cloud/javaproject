0ee570d2af5273bf2e06516b796c87bc7686bf8b
kusum <kusum@10.0.0.10>
2021-03-26 22:57:18 -0400

    Up loading all filesa

---
+++ javaproject/src/main/java/LocalVariables.java
@@ -0,0 +1,11 @@
+
+public class LocalVariables {
+
+	public static void main(String[] args) {
+		
+
+	
+public
+
+int int faruq=23;
+System.out.println(faruq);
---
+++ javaproject/src/main/java/instancevariables/InstanceVariables.java
@@ -0,0 +1,12 @@
+package instancevariables;
+
+public class InstanceVariables {
+String name="Zubaida";
+static int age=5;	
+	
+	public static void main(String[] args) {
+System.out.println(age)	;	
+
+	}
+
+}
