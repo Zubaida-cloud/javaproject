5854e2860156dc520346cb17257e647b3a2683ad
kusum <kusum@10.0.0.10>
2021-03-26 23:11:21 -0400

    uploading all files

---
+++ javaproject/src/main/java/LocalVariable.java
@@ -0,0 +1,27 @@
+
+public class LocalVariable {
+String myVar1="zubaida";
+	
+	
+	
+	
+	
+public static void	 myMethod() {
+String myVar="Local Variable";
+System.out.println(myVar);
+
+
+
+}
+	
+	
+	
+	public static void main(String[] args) {
+		LocalVariable obj=new LocalVariable();
+System.out.println(obj.myVar1);		
+		
+		
+myMethod();
+	}
+
+}
