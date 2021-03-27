28da483654bf4d4074bc28a9835a7d7eabe10464
kusum <kusum@10.0.0.10>
2021-03-26 23:44:54 -0400

    upload all files

---
+++ javaproject/src/main/java/variables/LocalVriables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class LocalVriables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+public void age() {
+int faruq=20;
+System.out.println{faruq};
